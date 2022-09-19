1138dfbeaa00014d94b4b53e1a8ec61e8dc6cf32
GNaD <cpt.meoz@gmail.com>
2022-09-19 20:51:19 +0700

    add test send ubedrock

--- app/ante_gate_test.go
+++ app/ante_gate_test.go
@@ -12,8 +12,11 @@ import (
 	"github.com/cosmos/cosmos-sdk/testutil"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	bank "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
+	"github.com/Pylons-tech/pylons/app"
 	"github.com/Pylons-tech/pylons/testutil/network"
+	"github.com/Pylons-tech/pylons/x/pylons/types"
 	clitestutil "github.com/cosmos/cosmos-sdk/testutil/cli"
 	"github.com/stretchr/testify/require"
 )
@@ -70,6 +73,58 @@ func TestOtherTransactionIsValid(t *testing.T) {
 
 	require.NoError(t, ctx.Codec.UnmarshalJSON(validRes.Bytes(), &resp))
 	require.Equal(t, successCode, resp.Code)
+}
+
+func TestSendUbedrock(t *testing.T) {
+
+	for _, tc := range []struct {
+		desc      string
+		sender    string
+		receiver  string
+		shouldErr bool
+	}{
+		{
+			desc:      "Valid",
+			sender:    app.Address1,
+			receiver:  app.Address2,
+			shouldErr: false,
+		},
+	} {
+		tc := tc
+		t.Run(tc.desc, func(t *testing.T) {
+			net := setupTestGenesis(t)
+
+			ctx := net.Validators[0].ClientCtx
+			sender := net.Validators[0].Address.String()
+			_ = sender
+
+			common := CommonArgs(net)
+			args := []string{sender, tc.receiver, "1ubedrock"}
+			args = append(args, common...)
+			res, _ := clitestutil.ExecTestCLICmd(ctx, bank.NewSendTxCmd(), args)
+			net.WaitForNextBlock()
+			var resp sdk.TxResponse
+			require.NoError(t, ctx.Codec.UnmarshalJSON(res.Bytes(), &resp))
+			require.Equal(t, successCode, resp.Code)
+		})
+	}
+}
+
+func setupTestGenesis(t *testing.T) *network.Network {
+	config := network.DefaultConfig()
+	config.MinGasPrices = fmt.Sprintf("0%s", types.StakingCoinDenom)
+	config.BondDenom = types.StakingCoinDenom
+
+	stakingParams := stakingtypes.DefaultParams()
+	stakingParams.BondDenom = types.StakingCoinDenom
+	state := stakingtypes.GenesisState{}
+	require.NoError(t, config.Codec.UnmarshalJSON(config.GenesisState[stakingtypes.ModuleName], &state))
+	state.Params = stakingParams
+	buffer, err := config.Codec.MarshalJSON(&state)
+	require.NoError(t, err)
+	config.GenesisState[stakingtypes.ModuleName] = buffer
+
+	return network.New(t, config)
 }
 
 func CommonArgs(net *network.Network) []string {
